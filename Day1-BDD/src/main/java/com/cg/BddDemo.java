c1d549124f68c5221eb4f175323052f792b85a32
snbaredd <snbaredd@DIN73003132.corp.capgemini.com>
2018-11-23 15:47:21 +0530

    second commit

--- Day1-BDD/src/main/java/com/cg/BddDemo.java
+++ Day1-BDD/src/main/java/com/cg/BddDemo.java
@@ -4,7 +4,13 @@ public class BddDemo {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		int sum = add(12,13);
 
+	}
+
+	private static int add(int i, int j) {
+		
+		return i+j;
 	}
 
 }
